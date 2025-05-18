7487da560afaa4afa40efffbe0299a74f81a0092
Suyash Pandey <132772402+suyash1518@users.noreply.github.com>
2025-05-18 22:24:35 +0530

    Time: 1 ms (86.03%), Space: 40.8 MB (83.15%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,6 +1,19 @@
 class Solution {
-    public int mySqrt(int x) {
-        int sqrt=(int) Math.sqrt(x);
-        return sqrt;
+   public int mySqrt(int x) {
+    if (x < 2) return x; 
+
+    int start = 1, end = x / 2;
+    while (start <= end) {
+        int mid = start + (end - start) / 2;
+
+        if (mid == x / mid) {
+            return mid;
+        } else if (mid < x / mid) {
+            start = mid + 1;
+        } else {
+            end = mid - 1;
+        }
     }
+    return end; 
+}
 }
