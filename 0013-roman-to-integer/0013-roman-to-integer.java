377c8615b865b51a19c9ff4e2b92961d6aa9004c
Suyash Pandey <132772402+suyash1518@users.noreply.github.com>
2025-06-10 20:08:36 +0530

    Time: 5 ms (53.79%), Space: 45 MB (24.48%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,32 +1,26 @@
 class Solution {
     public int romanToInt(String s) {
-        // Map Roman numerals to their integer values
-        Map<Character, Integer> romanMap = new HashMap<>();
-        romanMap.put('I', 1);
-        romanMap.put('V', 5);
-        romanMap.put('X', 10);
-        romanMap.put('L', 50);
-        romanMap.put('C', 100);
-        romanMap.put('D', 500);
-        romanMap.put('M', 1000);
-        
-        // Initialize result
-        int total = 0;
-        
-        // Loop through the string
-        for (int i = 0; i < s.length(); i++) {
-            // Get the value of the current character
-            int currentVal = romanMap.get(s.charAt(i));
-            
-            // If the current value is less than the next value, subtract it
-            if (i < s.length() - 1 && currentVal < romanMap.get(s.charAt(i + 1))) {
-                total -= currentVal;
-            } else {
-                // Otherwise, add the value
-                total += currentVal;
-            }
+        HashMap <Character, Integer> map= new HashMap<>();
+        int result=0;
+        map.put('I',1);
+        map.put('V',5);
+        map.put('X',10);
+        map.put('L',50);
+        map.put('C',100);
+        map.put('D',500);
+        map.put('M',1000);
+
+        int n=s.length();
+
+        for(int i=0; i<n; i++){
+             if (i < n - 1 && map.get(s.charAt(i)) < map.get(s.charAt(i + 1))){
+                result -= map.get(s.charAt(i));
+             }
+
+             else{
+            result += map.get(s.charAt(i));
+             }
         }
-        
-        return total;
+        return result;
     }
 }
