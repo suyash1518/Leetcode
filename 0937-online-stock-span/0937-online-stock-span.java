94ba78f987ed0277384002f1fbbef7107c528f7b
Suyash Pandey <132772402+suyash1518@users.noreply.github.com>
2025-07-10 10:43:49 +0530

    Time: 30 ms (62.73%), Space: 55.9 MB (13.04%) - LeetHub

--- 0937-online-stock-span/0937-online-stock-span.java
+++ 0937-online-stock-span/0937-online-stock-span.java
@@ -1,23 +1,16 @@
 class StockSpanner {
-
-    // Stack stores pairs of [price, span]
     Stack<int[]> s;
 
     public StockSpanner() {
-        s = new Stack<>();
+        s= new Stack<>();
     }
 
     public int next(int price) {
-        int span = 1;
-
-        // Pop all prices less than or equal to current price
-        while (!s.isEmpty() && s.peek()[0] <= price) {
-            span += s.pop()[1];
+        int span=1;
+        while(!s.isEmpty() && s.peek()[0] <= price){
+            span+= s.pop()[1];
         }
-
-        // Push current price and its span
         s.push(new int[]{price, span});
-
         return span;
     }
 }
